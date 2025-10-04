80f728eb709f3db0b55dfc9ff1fb8dfbfd54f8f8
t-liu <thomas.s.liu@gmail.com>
2025-10-04 18:27:47 -0400

    convert personal website from static html to vue.js app

---
+++ src/components/Navigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="nav nav--fixed">
+    <router-link to="/" class="nav__logo w-inline-block"></router-link>
+    <div class="nav__transition-block w-hidden-main"></div>
+    <div class="nav__transition-block-2 w-hidden-main"></div>
+    <div 
+      @click="toggleMobileMenu" 
+      class="nav-hamburger-btn w-hidden-main"
+      :class="{ 'w--open': mobileMenuOpen }"
+    >
+      <div class="line-1"></div>
+      <div class="line-2"></div>
+      <div class="line-3"></div>
+    </div>
+    <div class="nav__container" :class="{ 'w--open': mobileMenuOpen }">
+      <div class="nav__inner-container">
+        <div class="nav__sub-container">
+          <router-link to="/" class="nav__item">Portfolio</router-link>
+          <router-link to="/about" class="nav__item">About</router-link>
+          <a href="https://resume.thomasliu.click" class="nav__item" target="_blank">Résumé</a>
+          <a href="https://www.linkedin.com/in/thomas-liu-tech" target="_blank" class="company__social-links w-inline-block">
+            <img src="/images/icons/linkedin.png" class="company__social-icons" alt="LinkedIn" />
+          </a>
+          <a href="https://twitter.com/tliu301" target="_blank" class="company__social-links w-inline-block">
+            <img src="/images/icons/twitter.png" class="company__social-icons" alt="Twitter" />
+          </a>
+          <a href="https://github.com/t-liu" target="_blank" class="company__social-links w-inline-block">
+            <img src="/images/icons/github.png" class="company__social-icons" alt="GitHub" />
+          </a>
+          <a href="mailto:thomas.s.liu@gmail.com?subject=Hey%20T-Liu%2C%20I%20Found%20You%20on%20the%20Internet" class="company__social-links w-inline-block">
+            <img src="/images/icons/mail.png" class="company__social-icons" alt="Email" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      mobileMenuOpen: false
+    }
+  },
+  methods: {
+    toggleMobileMenu() {
+      this.mobileMenuOpen = !this.mobileMenuOpen
+    }
+  },
+  watch: {
+    '$route'() {
+      // Close mobile menu when route changes
+      this.mobileMenuOpen = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Navigation specific styles are in the main CSS file */
+</style>
+
+
---
+++ src/components/SideNavigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="side-nav">
+    <div class="side-nav-container">
+      <a href="#" @click.prevent="scrollToPortfolio" class="btn-primary btn-primary--side-nav w-button" :class="{ 'w--current': $route.path === '/' }">
+        Portfolio
+      </a>
+      <router-link to="/about" class="btn-primary btn-primary--side-nav w-button" :class="{ 'w--current': $route.path === '/about' }">
+        About
+      </router-link>
+      <a href="https://resume.thomasliu.click" target="_blank" class="btn-primary btn-primary--side-nav w-button">        
+        Résumé
+        <img src="/images/icons/new_tab.png" alt="New Tab Icon" class="icon">
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideNavigation',
+  methods: {
+    scrollToPortfolio() {
+      // If we're not on the home page, navigate there first
+      if (this.$route.path !== '/') {
+        this.$router.push('/')
+        // Wait for navigation to complete, then scroll
+        this.$nextTick(() => {
+          this.performScroll()
+        })
+      } else {
+        // We're already on home page, just scroll
+        this.performScroll()
+      }
+    },
+    performScroll() {
+      const portfolioElement = document.getElementById('portfolio')
+      if (portfolioElement) {
+        portfolioElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Side navigation styles are in the main CSS file */
+</style>
+
+
