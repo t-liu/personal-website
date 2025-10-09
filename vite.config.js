4c762732ed4058243547a5c95919042e22003aa2
t-liu <thomas.s.liu@gmail.com>
2025-10-09 15:26:03 -0400

    update img src to cloudinary for home and about page

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [vue()],
+    server: {
+      port: 3000,
+      open: true
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets'
+    },
+    define: {
+      'process.env': env
+    }
   }
 })
 
