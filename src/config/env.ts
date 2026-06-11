edfa48d29242cace6f5e686ef5b090c6a1c76dfa
t-liu <thomas.s.liu@gmail.com>
2026-06-10 23:39:49 -0400

    give socialLinks an explicit interface instead of relying on as const

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,35 +1,24 @@
 // src/config/env.ts
+import type { IconProp } from '@fortawesome/fontawesome-svg-core'
+
+interface SocialLink {
+  label: string
+  url: string
+  icon: IconProp
+  ariaLabel: string
+}
+
+const socialLinks: SocialLink[] = [
+  { label: 'LinkedIn', url: 'https://www.linkedin.com/in/thomas-liu-tech', icon: ['fab', 'linkedin'], ariaLabel: 'Visit my LinkedIn profile' },
+  { label: 'Twitter', url: 'https://twitter.com/tliu301', icon: ['fab', 'twitter'], ariaLabel: 'Visit my Twitter page' },
+  { label: 'GitHub', url: 'https://github.com/t-liu', icon: ['fab', 'github'], ariaLabel: 'Visit my GitHub page' },
+  { label: 'Email', url: 'mailto:thomas.s.liu@gmail.com?subject=Hey%20T-Liu%2C%20I%20Found%20You%20on%20the%20Internet', icon: ['far', 'envelope'], ariaLabel: 'Send me an email' },
+]
+
 export const config = {
   cloudinaryBaseUrl: import.meta.env.VITE_CLOUDINARY_BASE_URL || 'https://res.cloudinary.com/decbhr3np/image/upload/f_auto,q_auto',
   formspreeEndpoint: 'https://formspree.io/f/mldwoogl',
   jokeApiUrl: 'https://icanhazdadjoke.com',
   resumeUrl: 'https://resume.thomasliu.click',
-  socialLinks: 
-    [
-      {
-        label: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/thomas-liu-tech',
-        icon: ['fab', 'linkedin'],
-        ariaLabel: 'Visit my LinkedIn profile'
-      },
-      {
-        label: 'Twitter',
-        url: 'https://twitter.com/tliu301',
-        icon: ['fab', 'twitter'],
-        ariaLabel: 'Visit my Twitter page'
-      },
-      {
-        label: 'GitHub',
-        url: 'https://github.com/t-liu',
-        icon: ['fab', 'github'],
-        ariaLabel: 'Visit my GitHub page'
-      },
-      {
-        label: 'Email',
-        url:
-          'mailto:thomas.s.liu@gmail.com?subject=Hey%20T-Liu%2C%20I%20I%20Found%20You%20on%20the%20Internet',
-        icon: ['far', 'envelope'],
-        ariaLabel: 'Send me an email'
-      },
-    ],
-  } as const
+  socialLinks
+} as const
